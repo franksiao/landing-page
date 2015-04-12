d82466bc9ef906564972c22725090881c69debeb
Frank Siao <fsiao@appnexus.com>
2015-04-12 16:35:22 -0400

    email workin

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser')
 var app = express();
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'bambamanalytics@gmail.com',
+        pass: 'chowchow123'
+    }
+});
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.post('/send-email', function(request, response) {
+	var data = JSON.parse(request.body.data);
+	var htmlText =
+		'<p>Name: ' + data.name + '</p>' +
+		'<p>Email: ' + data.email + '</p>' + 
+		'<p>Phone: ' + data.phone + '</p>' +
+		'<p>Message: ' + data.message + '</p>';
+	var mailOptions = {
+	    from: 'bambamanalytics@gmail.com', // sender address
+	    to: 'franksiao@gmail.com, joseph.cal@gmail.com ', // list of receivers
+	    subject: 'Client interest from ' + data.name, // Subject line
+	    html: htmlText // html body
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+	        console.log(error);
+	        response.send('Error');
+	    }else{
+	        console.log('Message sent: ' + info.response);
+	        response.send('Cool');
+	    }
+	});
+  
 });
 
 app.listen(app.get('port'), function() {
--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -12,21 +12,23 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var data = {
+                name: name,
+                email: email,
+                phone: phone,
+                message: message
+            };
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "send-email",
                 type: "POST",
                 data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                    data: JSON.stringify(data)
                 },
-                cache: false,
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
